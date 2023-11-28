0cfe6679394e58f36c9d3c00a36b5063556391b9
posh <101807149+ImPosh@users.noreply.github.com>
2023-11-28 11:51:37 -0500

    Update uv.config.js

--- Unblock/uv/uv.config.js
+++ Unblock/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'some sort of bare server',
+    bare: 'https://savethetrees.sen.org.nz/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
